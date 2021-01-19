84aa01c1d7c2d58db8e3a1f5a190cdd84a3c7e82
louis11s <losc714@gmail.com>
2021-01-19 23:28:43 +0100

    For real for real final push

--- M133-Webshop BBZW/frontend/scripts/header.js
+++
@@ -1,6 +0,0 @@
-const header = document.createElement("header");
-header.innerHTML = "<span>Dorfladen</span><span>BBZW</span>";
-const bodyElement = document.querySelector("body");
-if (bodyElement) {
-    bodyElement.prepend(header);
-}
